4e2b9841d11d077881a41abae5a2ce11e6c27ede
Zhoushiya <zhoushiya@126.com>
2020-05-12 09:47:53 +0800

    一行代码实现Restful接口

--- mongodb/src/main/java/com/example/mongodb/dao/IUserDao.java
+++ mongodb/src/main/java/com/example/mongodb/dao/IUserDao.java
@@ -2,7 +2,9 @@ package com.example.mongodb.dao;
 
 import com.example.mongodb.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "user",path="users")
 public interface IUserDao extends MongoRepository<User,String> {
     /**
      * 通过用户名查询，和JPA一样可以findBy属性自动查询
