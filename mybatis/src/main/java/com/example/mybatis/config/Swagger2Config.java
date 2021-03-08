f09fa1fa8b98f9e649f40ed7b49358d3cfa06224
周诗雅 <zhoushiya@126.com>
2021-03-08 17:09:16 +0800

    修改basePackage

--- mybatis/src/main/java/com/example/mybatis/config/Swagger2Config.java
+++ mybatis/src/main/java/com/example/mybatis/config/Swagger2Config.java
@@ -12,12 +12,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class Swagger2 {
+public class Swagger2Config {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("springboot利用swagger构建api文档")
-                .description("简单优雅的restfun风格")
+                .description("简单优雅的restful风格")
                 .termsOfServiceUrl("https://www.cnblogs.com/zhoushiya")
                 .version("1.0")
                 .build();
@@ -29,7 +29,7 @@ public class Swagger2 {
                 .apiInfo(apiInfo())
                 .select()
                 //扫描basePackage包下面的“/rest/”路径下的内容作为接口文档构建的目标
-                .apis(RequestHandlerSelectors.basePackage("com.zhoushiya.springbootstudy"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.mybatis"))
                 .paths(PathSelectors.regex("/rest/.*"))
                 .build();
     }
